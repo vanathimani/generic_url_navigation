cd2709652e77d9268b222c41a4e4dc22d97c6a21
vanathimani <git config --global user.email vanathimani9@gmail.com>
2023-08-01 14:59:50 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def vanu(request):
+    return render(request,'vanu.html')
+
+def asritha(request):
+    return render(request,'asritha.html')
